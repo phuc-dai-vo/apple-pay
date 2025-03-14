44880174f1fb63fd6b1f241ace2a9c89a8c7d7bf
Phuc.Vo <phuc.vo@steadfasttech.com.au>
2025-03-14 11:12:01 +0700

    a

--- src/App.js
+++ src/App.js
@@ -15,33 +15,37 @@ function App() {
   };
 
   const _applePayCapabilities = async () => {
-    try {
-      const _applePayCapabilities = await window.ApplePaySession.applePayCapabilities(option);
-      setApplePayCapabilities(_applePayCapabilities);
-    } catch (error) {
-      setApplePayCapabilities(error);
+    if (window.ApplePaySession.applePayCapabilities) {
+      try {
+        const _applePayCapabilities = await window.ApplePaySession.applePayCapabilities(option);
+        setApplePayCapabilities(_applePayCapabilities);
+      } catch (error) {
+        setApplePayCapabilities(error);
+      }
     }
   }
 
   const _canMakePayments = async () => {
-    try {
-      const _canMakePayments = await window.ApplePaySession.canMakePayments(option);
-      setCanMakePayments(_canMakePayments);
-    } catch (error) {
-      setCanMakePayments(error);
+    if (window.ApplePaySession.canMakePayments) {
+      try {
+        const _canMakePayments = await window.ApplePaySession.canMakePayments(option);
+        setCanMakePayments(_canMakePayments);
+      } catch (error) {
+        setCanMakePayments(error);
+      }
     }
-
   }
 
   const _canMakePaymentsWithActiveCard = async () => {
-    try {
-      const _canMakePaymentsWithActiveCard = await window.ApplePaySession.canMakePaymentsWithActiveCard(option);
-      setCanMakePaymentsWithActiveCard(_canMakePaymentsWithActiveCard);
-    } catch (error) {
-      setCanMakePaymentsWithActiveCard(error);
+    if (window.ApplePaySession.canMakePaymentsWithActiveCard) {
+      try {
+        const _canMakePaymentsWithActiveCard = await window.ApplePaySession.canMakePaymentsWithActiveCard(option);
+        setCanMakePaymentsWithActiveCard(_canMakePaymentsWithActiveCard);
+      } catch (error) {
+        setCanMakePaymentsWithActiveCard(error);
+      }
     }
   }
-
 
   useEffect(() => {
     if (window.ApplePaySession) {
@@ -72,12 +76,12 @@ function App() {
                     <tr>
                       <th scope="row">1</th>
                       <td>ApplePaySession.applePayCapabilities</td>
-                      <td>{applePayCapabilities|| 'NA'}</td>
+                      <td>{applePayCapabilities || 'NA'}</td>
                     </tr>
                     <tr>
                       <th scope="row">2</th>
                       <td>ApplePaySession.canMakePayments</td>
-                      <td>{canMakePayments|| 'NA'}</td>
+                      <td>{canMakePayments || 'NA'}</td>
                     </tr>
                     <tr>
                       <th scope="row">3</th>
