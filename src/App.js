5c67bfba59af3d2b63f801f1442b58e67548ebb3
Phuc.Vo <phuc.vo@steadfasttech.com.au>
2025-03-14 11:41:53 +0700

    Update code

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     if (window.ApplePaySession.canMakePayments) {
       try {
         const _canMakePayments = window.ApplePaySession.canMakePayments(option);
-        setCanMakePayments(_canMakePayments);
+        setCanMakePayments(_canMakePayments ? 'TRUE' : 'FALSE');
       } catch (error) {
         setCanMakePayments('ERROR');
       }
